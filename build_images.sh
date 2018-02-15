3ad5f5a534c3960c222cc9783ee1ed8cdc314ecb
Faruk D <f.diblen@esciencecenter.nl>
2018-02-15 17:01:36 +0100

    add build script

---
+++ build_images.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# https://docs.docker.com/engine/reference/commandline/build/#squash-an-images-layers-squash-experimental-only
+
+version=$(date +"%d%m%Y")
+
+#export DOCKER_ID_USER=$(whoami)
+#docker login
+
+#for folder in $(ls -d */)
+for folder in \
+	spark-hadoop-base \
+	hadoop \
+	spark-master \
+	spark-worker \
+	spark-master-dirac \
+	spark-worker-dirac
+do
+    name=${folder%%/}
+    echo
+    echo "==========================================="
+    echo "Building: "$name
+    echo "==========================================="
+
+
+    cmd="docker build -t fdiblen/$name:$version -t fdiblen/$name:latest ./$name"
+    echo
+    echo "Running:"
+    echo $cmd
+    echo
+    echo
+    eval $cmd
+
+    if [ $? -eq 0 ]
+    then
+      echo
+      echo "Successfully built the $name image!"
+    else
+      echo
+      echo "Could not built the $name image" >&2
+      exit $?
+    fi
+
+
+    ## https://docs.docker.com/docker-cloud/builds/push-images/
+    #docker push fdiblen/$name:$version
+done
