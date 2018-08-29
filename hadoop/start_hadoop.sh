2b201c159d5408d955698219d9ae0547380be97e
Faruk D <f.diblen@esciencecenter.nl>
2018-08-29 11:54:25 +0200

    fixes: - java environment - log files - initial hadoop dataset

--- hadoop/start_hadoop.sh
+++ hadoop/start_hadoop.sh
@@ -14,12 +14,19 @@
 # /opt/soft/hadoop/sbin/start-yarn.sh
 
 
+
+export PATH=$PATH:/usr/lib/jvm/java-8-openjdk/bin
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk
+export HADOOP_CLASSPATH=$(/opt/soft/hadoop/bin/hadoop classpath)
+export PATH=$PATH:/opt/soft/hadoop/bin
+
 echo
 echo "Creating user directories"
-/opt/soft/hadoop/bin/hdfs dfs -mkdir /user
-/opt/soft/hadoop/bin/hdfs dfs -mkdir /user/dirac
-/opt/soft/hadoop/bin/hdfs dfs -put /opt/data /user/dirac/
-/opt/soft/hadoop/bin/hdfs dfs -put titanic.csv /user/dirac/
+hdfs dfs -mkdir /user
+hdfs dfs -mkdir /user/dirac
+hdfs dfs -put /opt/dirac/datasets /user/dirac/
+hdfs dfs -put titanic.csv /user/dirac/
+hdfs dfs -ls /user/dirac/
 
 echo
 echo "Show Java Processes"
